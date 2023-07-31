2a6ad5d120b55baaf108e277c8509eb39d3f0f82
madyanes <madyanes.dev@gmail.com>
2023-07-31 15:32:42 +0700

    note detail feature added

--- src/components/NoteDetail.jsx
+++ src/components/NoteDetail.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { showFormattedDate } from '../utils/local-data'
 
 class NoteDetail extends React.Component {
   render() {
-    return <h1>Detail of {this.props.noteId}</h1>
+    return (
+      <>
+        <article className='note-detail'>
+          <header>
+            <h1>{this.props.note.title}</h1>
+          </header>
+          <footer>
+            <p>{showFormattedDate(this.props.note.createdAt)}</p>
+          </footer>
+          <p>{this.props.note.body}</p>
+        </article>
+      </>
+    )
   }
 }
 
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { getNote } from '../utils/local-data'
 import NoteDetail from '../components/NoteDetail'
 
 const DetailWrapper = () => {
@@ -9,9 +10,17 @@ const DetailWrapper = () => {
 }
 
 class Detail extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      note: getNote(this.props.id),
+    }
+  }
+
   render() {
     return (
-      <NoteDetail noteId={this.props.id} />
+      <NoteDetail note={this.state.note} />
     )
   }
 }
