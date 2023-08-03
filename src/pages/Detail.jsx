498338c800311de149b86c0344073bdb0287313b
madyanes <madyanes.dev@gmail.com>
2023-08-03 10:54:58 +0700

    prop types validation for <Detail />

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import PropTypes from 'prop-types'
 import NoteDetail from '../components/NoteDetail'
 
 const DetailWrapper = ({ deleteNoteHandler, archiveNoteHandler, getNoteHandler }) => {
@@ -18,6 +19,18 @@ class Detail extends React.Component {
       />
     )
   }
+}
+
+DetailWrapper.propTypes = {
+  getNoteHandler: PropTypes.func.isRequired,
+  deleteNoteHandler: PropTypes.func.isRequired,
+  archiveNoteHandler: PropTypes.func.isRequired,
+}
+
+Detail.propTypes = {
+  deleteNoteHandler: PropTypes.func.isRequired,
+  archiveNoteHandler: PropTypes.func.isRequired,
+  note: PropTypes.object.isRequired,
 }
 
 export default DetailWrapper
