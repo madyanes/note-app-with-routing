05c7884b408001088ed12ea0fe2e6c5404ed689a
madyanes <madyanes.dev@gmail.com>
2023-08-04 07:59:10 +0700

    stricter prop types validation merealisasikan feedback dari reviewer dicoding

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -35,9 +35,16 @@ class NoteList extends React.Component {
   }
 }
 
+const NoteItemPropTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  createdAt: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+}
+
 NoteList.propTypes = {
   headingText: PropTypes.string.isRequired,
-  notes: PropTypes.array.isRequired,
+  notes: PropTypes.arrayOf(PropTypes.shape(NoteItemPropTypes)).isRequired,
   deleteNoteHandler: PropTypes.func.isRequired,
   archiveNoteHandler: PropTypes.func.isRequired,
   searchKeyword: PropTypes.string.isRequired,
